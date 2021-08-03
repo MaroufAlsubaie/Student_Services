ba6836c1b02d8808321c3cf9e7c6668f4b7a0d91
Saad AlKathiri <s3d340@gmail.com>
2021-08-03 19:00:38 +0300

    product sort

--- Product.php
+++ Product.php
@@ -1,150 +1,133 @@
 <?php
     include_once 'header.php';
 ?>
+
+        <div class="container2">
+
+<form class="example" action="product.php" method="GET"><!--search-->
+     <input type="text" name="search" placeholder="وش نبي تبحث عنه...؟" style="text-align:right;">
+     <button  type="submit" style="padding: 2.5px;"><img src="images/search.png"></button>
+
+     <select name="sort" style="margin-left:890px; margin-bottom:20px ;">
+   <option value="">....الترتيب</option>
+   <option value="">عادي</option>
+   <option value="low">من الاقل للاعلى</option>
+   <option value="high">من الاعلى للاقل</option>
+   <option value="abc">من الالف للياء</option>
+   <option value="cba">من الياء للالف</option>
+   </select>
+
+<?php //type
+$query_t ="SELECT * FROM `product` ORDER BY `product`.`type` DESC";
+$type_q = mysqli_query($conn,$query_t);
+$temp = "";
+if (mysqli_num_rows($type_q)>0){?>
+   <p style=" text-align:right;"><?php
+   foreach($type_q as $typelist){?>
+       
+       <?php 
+       $checked = [];
+       if(isset($_GET['type'])){
+           $checked = $_GET['type'];
+       }
+
+        if($temp !== $typelist['type']){?>
+
+        <label style="margin-right:10px " > <?= $typelist['type'] ?></label>
+        <input style="margin-top:10px " type="checkbox" name="type[]"  value="<?= $typelist['type'] ?>"
+        <?php if(in_array($typelist['type'],$checked)){ echo "checked"; } ?>>
         
-             <div class="container2">
+        <br>
+        <?php
+        }
+        $temp = $typelist['type'];?>
+       <?php
+   }?></p><?php
+   
+}
+?>
 
-                 <form class="example" action="product.php" method="POST">
-                      <input type="text" name="search" placeholder="وش نبي تبحث عنه...؟" style="text-align:right;">
-                      <button  type="submit" name="submit-search" style="padding: 2.5px;"><img src="/images/search.png"></button>
-                    </form>
+<button type="submit" class="bottu1" > فلتر</button>
+</form>
 
-                    <br>
-                    <br>
-                    <br>
-                 <div class="goods">
-                     <?php
-                     $query ="SELECT * FROM product ORDER BY ID ASC ";
-                        if (isset($_GET["sort"])){
-                            if ($_GET["sort"] == "low")
-                            $query ="SELECT * FROM `product` ORDER BY `product`.`price` ASC";
-                            elseif ($_GET["sort"] == "high")
-                            $query ="SELECT * FROM `product` ORDER BY `product`.`price` DESC";
-                            elseif ($_GET["sort"] == "abc")
-                            $query ="SELECT * FROM `product` ORDER BY `product`.`Name` ASC";
-                            elseif ($_GET["sort"] == "cba")
-                            $query ="SELECT * FROM `product`  ORDER BY `product`.`Name` DESC";
+<div class="row">
 
-                        }
-                            ?>
 
-                 <form method="get">
-                   <select name="sort" style="margin-left:890px; margin-bottom:20px ;" onchange='if(this.value != 0) { this.form.submit(); }'>
-                    <option >....الترتيب</option>
-                    <option >عادي</option>
-                    <option value="low">من الاقل للاعلى</option>
-                    <option value="high">من الاعلى للاقل</option>
-                    <option value="abc">من الالف للياء</option>
-                    <option value="cba">من الياء للالف</option>
-                    </select>
-                </form>
-                <form method="get">
-                <?php 
-                $query_t ="SELECT * FROM `product` ORDER BY `product`.`type` DESC";
-                $type_q = mysqli_query($conn,$query_t);
-                $temp = "";
-                if (mysqli_num_rows($type_q)>0){?>
-                    <p style=" text-align:right;"><?php
-                    foreach($type_q as $typelist){?>
-                        
-                        <?php 
-                        $checked = [];
-                        if(isset($_GET['type'])){
-                            $checked = $_GET['type'];
-                        }
 
-                         if($temp !== $typelist['type']){?>
+<?php
+if (isset($_GET['search'])) 
+$search='%'.$_GET['search'].'%';
 
-                         <label style="margin-right:10px " > <?= $typelist['type'] ?></label>
-                         <input style="margin-top:10px " type="checkbox" name="type[]"  value="<?= $typelist['type'] ?>"
-                         <?php if(in_array($typelist['type'],$checked)){ echo "checked"; } ?>>
+else $search='%'.'%';
 
-                         <br>
-                         <?php
-                         }
-                         $temp = $typelist['type'];?>
-                        <?php
-                    }?></p><?php
-                    
-                }
-                ?>
-                
-                <button type="submit" class="bottu1" > فلتر</button>
-                </form>
-                   <div class="row">
-                   
-                   <?php 
-                    if(isset($_GET['type']))
-                    {
-                        $typechecked = [];
-                        $typechecked = $_GET['type'];
-                        foreach($typechecked as $rowbrand)
-                        {
-                            
-                            
-                    $so = [];
-                    if (isset($_GET["sort"])){
-                        $so = $_GET["sort"];
-                        if ($so == "low")
-                        $query ="SELECT * FROM `product` WHERE type = '$rowbrand' ORDER BY `product`.`price` ASC";
-                        elseif ($_GET["sort"] == "high")
-                        $query ="SELECT * FROM `product` WHERE type = '$rowbrand' ORDER BY `product`.`price` DESC";
-                        elseif ($_GET["sort"] == "abc")
-                        $query ="SELECT * FROM `product` WHERE type = '$rowbrand' ORDER BY `product`.`Name` ASC";
-                        elseif ($_GET["sort"] == "cba")
-                        $query ="SELECT * FROM `product` WHERE type = '$rowbrand' ORDER BY `product`.`Name` DESC";
-                        
-                    }
-                    else 
-                        $query ="SELECT * FROM `product` WHERE type = '$rowbrand' ORDER BY `ID` ASC";
-                }}
-                
-                if (isset($_POST['submit-search']) ){
-                    $search = mysqli_real_escape_string($conn, $_POST['search']);
-                    $query = "SELECT * FROM product WHERE Name LIKE '%$search%' OR photo LIKE '%$search%' OR type LIKE '%$search%'  ";
-                    
-                }
-                
-                
-                    $result = mysqli_query($conn,$query);
-                    while($row = mysqli_fetch_assoc($result)) {
-                        if($row["quantity"] > 0){
-                        ?>
-                    <div class="col-3">
-                        <div class="image">
-                        <form method="post" action="cart.php?id=<?=$row['ID']?>">
-                        <img src="images/<?=$row['photo']?>">
-                        <h4><?=$row['Name'];?></h4>
-                        <p><?=$row['price'];?> ريال</p>
-                        <input type="number" name="quantity" class="input1" value="1" min="1" max=<?php echo $row["quantity"]?>>
-                        <input type="hidden" name="hidden_name" value="<?php echo $row["Name"]?>">
-                        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]?>">
-                        <input type="submit" name="add" class="bottun1" value="اضف الى السلة" >
-                    </div>
-                    </form>
-                    </div>
-                    <?php }
-                    else {
-                        ?>
-                    <div class="col-3">
-                        <div class="image">
-                        <img src="images/<?=$row['photo']?>">
-                        <h4><?=$row['Name'];?></h4>
-                        <p><?=$row['price'];?> ريال</p>
-                        <input type="number" name="quantity" class="input1" value="<?php echo $row["quantity"]?>" min="0" max=<?php echo $row["quantity"]?>>
-                        <input type="hidden" name="hidden_name" value="<?php echo $row["Name"]?>">
-                        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]?>">
-                        <button class="bottun1" style="font-size:15px; background:rgb(107, 57, 0);" >نفذت الكمية</button>
-                    </div> 
-                    </div>
-                    <?php
-                    }    
-                } 
-            ?>
-                     
-                </div>
-             </div>
-            
-        </div>
+       $sql = "SELECT * FROM product WHERE(Name LIKE ? OR photo LIKE ? OR type LIKE ?)";
+
+       if(isset($_GET['type']))
+       {
+           $typechecked = [];
+           $typechecked = $_GET['type'];
+           foreach($typechecked as $rowbrand){
+            $sql = $sql."AND type in ('$rowbrand') ";
+           }}
+
+
+       if (isset($_GET["sort"])){
+        if ($_GET["sort"] == "low")
+        $sql =$sql."ORDER BY `product`.`price` ASC";
+        elseif ($_GET["sort"] == "high")
+        $sql =$sql."ORDER BY `product`.`price` DESC";
+        elseif ($_GET["sort"] == "abc")
+        $sql =$sql."ORDER BY `product`.`Name` ASC";
+        elseif ($_GET["sort"] == "cba")
+        $sql =$sql."ORDER BY `product`.`Name` DESC";
+       }
+       $stmt = mysqli_stmt_init($conn);
+       if (!mysqli_stmt_prepare($stmt, $sql)) {
+           echo "stmt eroor";
+           exit();
+       }
+
+        mysqli_stmt_bind_param($stmt, "sss", $search,$search,$search);
+
+       mysqli_stmt_execute($stmt);
+       $resultData = mysqli_stmt_get_result($stmt);
+       while($row = mysqli_fetch_assoc($resultData)) {
+        if($row["quantity"] > 0){
+        ?>
+    <div class="col-3">
+        <div class="image">
+        <form method="post" action="cart.php?id=<?=$row['ID']?>">
+        <img src="images/<?=$row['photo']?>">
+        <h4><?=$row['Name'];?></h4>
+        <p><?=$row['price'];?> ريال</p>
+        <input type="number" name="quantity" class="input1" value="1" min="1" max=<?php echo $row["quantity"]?>>
+        <input type="hidden" name="hidden_name" value="<?php echo $row["Name"]?>">
+        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]?>">
+        <input type="submit" name="add" class="bottun1" value="اضف الى السلة" >
+    </div>
+    </form>
+    </div>
+    <?php }
+    else {
+        ?>
+    <div class="col-3">
+        <div class="image">
+        <img src="images/<?=$row['photo']?>">
+        <h4><?=$row['Name'];?></h4>
+        <p><?=$row['price'];?> ريال</p>
+        <input type="number" name="quantity" class="input1" value="<?php echo $row["quantity"]?>" min="0" max=<?php echo $row["quantity"]?>>
+        <input type="hidden" name="hidden_name" value="<?php echo $row["Name"]?>">
+        <input type="hidden" name="hidden_price" value="<?php echo $row["price"]?>">
+        <button class="bottun1" style="font-size:15px; background:rgb(107, 57, 0);" >نفذت الكمية</button>
+    </div> 
+    </div>
+    <?php
+    }
+} 
+?>
+
+</div>
+</div>
+</div>
     </body>
 </html>
